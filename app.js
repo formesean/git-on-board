22d22e0b3e267045f56be1a150d5a7e0318efb54
formesean <seanaguilar698@gmail.com>
2025-01-10 20:55:39 +0800

    feat: update welcome messaages for git merge demo

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to Git in Sync!");
+    res.send("Learn Git Merging!");
 });
 
 const server = app.listen(3000, () => {
-    console.log("Listening to port 8080.");
+    console.log("Listening to port 3000.");
 });
 
 module.exports = server;
